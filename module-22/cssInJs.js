d72d3bc2899c4e4bec4bc9b609f90d0017d44002
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-02-14 19:30:30 +0600

    new

---
+++ module-22/cssInJs.js
@@ -0,0 +1,7 @@
+// const sections = document.getElementsByTagName('section');
+const sections = document.querySelectorAll('section');
+// console.log(sections);
+
+for(const section of sections){
+    console.log(section);
+}
